5f355afd3957138da56393b7bfaff4f7be0df2e0
Syfaro <syfaro@huefox.com>
2018-11-06 20:23:46 -0600

    Support server query.

--- src/lib.rs
+++ src/lib.rs
@@ -59,6 +59,15 @@ fn u16_to_u8(num: u16) -> [u8; 2] {
     [((num >> 8) & 0xFF) as u8, (num & 0xFF) as u8]
 }
 
+fn u32_to_u8(num: u32) -> [u8; 4] {
+    [
+        ((num >> 24) & 0xFF) as u8,
+        ((num >> 16) & 0xFF) as u8,
+        ((num >> 8) & 0xFF) as u8,
+        (num & 0xFF) as u8,
+    ]
+}
+
 fn build_packet(data: Vec<u8>, id: u32) -> Vec<u8> {
     let id = encode_varint(id);
     let len = encode_varint((data.len() + id.len()) as u32);
@@ -86,7 +95,7 @@ fn build_handshake(host: &str, port: u16) -> Vec<u8> {
 
 #[cfg(unix)]
 fn get_dns_config() -> Result<resolve::DnsConfig, resolve::Error> {
-    resolve::DnsConfig::load_default()?
+    Ok(resolve::DnsConfig::load_default()?)
 }
 
 #[cfg(windows)]
@@ -209,6 +218,15 @@ impl From<serde_json::error::Error> for Error {
     }
 }
 
+impl From<std::num::ParseIntError> for Error {
+    fn from(error: std::num::ParseIntError) -> Self {
+        Self {
+            message: error.to_string(),
+            bad_server: false,
+        }
+    }
+}
+
 fn resolve(host: &str, port: u16) -> Result<std::net::SocketAddr, Error> {
     let resolved = resolve_srv(&host);
 
@@ -226,10 +244,12 @@ fn resolve(host: &str, port: u16) -> Result<std::net::SocketAddr, Error> {
 
     match addrs.next() {
         Some(addr) => Ok(addr),
-        None => return Err(Error {
-            message: "unable to resolve".to_string(),
-            bad_server: true,
-        })
+        None => {
+            return Err(Error {
+                message: "unable to resolve".to_string(),
+                bad_server: true,
+            })
+        }
     }
 }
 
@@ -262,9 +282,161 @@ pub fn send_ping(host: &str, port: u16) -> Result<Ping, Error> {
     Ok(ping)
 }
 
+fn parse_plugins(plugins: Option<String>) -> (String, Vec<String>) {
+    let plugins = match plugins {
+        None => return ("".to_string(), vec![]),
+        Some(plugins) => plugins,
+    };
+
+    let mut parts = plugins.split(": ");
+
+    let server_mod_name = parts.next().unwrap();
+    let plugins: Vec<String> = match parts.next() {
+        Some(plugins) => plugins
+            .split("; ")
+            .map(|plugin| plugin.to_string())
+            .collect(),
+        None => vec![],
+    };
+
+    return (server_mod_name.to_string(), plugins);
+}
+
+#[derive(Debug, Serialize)]
+pub struct Query {
+    pub hostname: String,
+    pub gametype: String,
+    pub game_id: String,
+    pub version: String,
+    pub plugins: (String, Vec<String>),
+    pub map: String,
+    pub numplayers: usize,
+    pub maxplayers: usize,
+    pub hostport: u16,
+    pub hostip: String,
+    pub players: Vec<String>,
+}
+
+fn string_until_zero<T>(reader: &mut T) -> Option<String>
+where
+    T: Read,
+{
+    let mut items: Vec<u8> = vec![];
+
+    let mut buf = [0; 1];
+    loop {
+        if reader.read_exact(&mut buf).is_err() {
+            return None;
+        }
+
+        match buf[0] {
+            0x00 => break,
+            _ => items.push(buf[0]),
+        }
+    }
+
+    if items.is_empty() {
+        return None;
+    }
+
+    Some(String::from_utf8_lossy(&items).to_string())
+}
+
+fn string_until_zero_expected<T>(mut reader: &mut T, expected: &str) -> Option<String>
+where
+    T: Read,
+{
+    let key = match string_until_zero(&mut reader) {
+        Some(key) => key,
+        None => return None,
+    };
+
+    if &key != expected {
+        return None;
+    }
+
+    string_until_zero(&mut reader)
+}
+
+fn parse_players<T>(mut reader: &mut T) -> Vec<String>
+where
+    T: Read,
+{
+    let mut players = vec![];
+
+    let mut _garbage = vec![0; 11];
+    let _err = reader.read_exact(&mut _garbage);
+
+    loop {
+        match string_until_zero(&mut reader) {
+            Some(player) => players.push(player),
+            None => break,
+        }
+    }
+
+    players
+}
+
+pub fn send_query(host: &str, port: u16) -> Result<Query, Error> {
+    use std::net::UdpSocket;
+
+    let conn = resolve(host, port)?;
+
+    let socket = UdpSocket::bind("127.0.0.1:0")?;
+    socket.connect(conn)?;
+
+    let session_id = rand::random::<u32>() & 0x0F0F0F0F;
+
+    let mut request = vec![0xFE, 0xFD, 0x09];
+    request.extend(&u32_to_u8(session_id));
+
+    socket.send(&request)?;
+
+    let mut buf: Vec<u8> = vec![0; 2048];
+    let len = socket.recv(&mut buf)?;
+
+    let challenge_token: i32 = String::from_utf8_lossy(&buf[5..len - 1]).parse()?;
+
+    let mut request = vec![0xFE, 0xFD, 0x00];
+    request.extend(&u32_to_u8(session_id));
+    request.extend(&u32_to_u8(challenge_token as u32));
+    request.extend(vec![0x00, 0x00, 0x00, 0x00]);
+
+    socket.send(&request)?;
+
+    let len = socket.recv(&mut buf)?;
+    let mut cursor = std::io::Cursor::new(&buf[16..len - 1]);
+
+    Ok(Query {
+        hostname: string_until_zero_expected(&mut cursor, "hostname").unwrap(),
+        gametype: string_until_zero_expected(&mut cursor, "gametype").unwrap(),
+        game_id: string_until_zero_expected(&mut cursor, "game_id").unwrap(),
+        version: string_until_zero_expected(&mut cursor, "version").unwrap(),
+        plugins: parse_plugins(string_until_zero_expected(&mut cursor, "plugins")),
+        map: string_until_zero_expected(&mut cursor, "map").unwrap(),
+        numplayers: string_until_zero_expected(&mut cursor, "numplayers")
+            .unwrap()
+            .parse()
+            .unwrap_or(0),
+        maxplayers: string_until_zero_expected(&mut cursor, "maxplayers")
+            .unwrap()
+            .parse()
+            .unwrap_or(0),
+        hostport: string_until_zero_expected(&mut cursor, "hostport")
+            .unwrap()
+            .parse()
+            .unwrap_or(0),
+        hostip: string_until_zero_expected(&mut cursor, "hostip").unwrap(),
+        players: parse_players(&mut cursor),
+    })
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{build_packet, encode_varint, read_varint, resolve_srv, send_ping};
+    use super::{
+        build_packet, encode_varint, parse_players, parse_plugins, read_varint, resolve_srv,
+        send_ping, send_query, string_until_zero, string_until_zero_expected,
+    };
 
     #[test]
     fn test_encode_varint() {
@@ -339,15 +511,77 @@ mod tests {
     }
 
     #[test]
+    fn test_string_until_zero() {
+        let mut cursor = std::io::Cursor::new(vec![102, 111, 120, 0, 104, 105, 0]);
+
+        let msg = string_until_zero(&mut cursor);
+
+        assert!(!msg.is_none());
+        assert_eq!(msg.unwrap(), "fox");
+
+        let msg = string_until_zero(&mut cursor);
+
+        assert!(!msg.is_none());
+        assert_eq!(msg.unwrap(), "hi");
+
+        let msg = string_until_zero(&mut cursor);
+
+        assert!(msg.is_none());
+    }
+
+    #[test]
+    fn test_string_until_zero_expected() {
+        let mut cursor = std::io::Cursor::new(vec![107, 0, 118, 0]);
+
+        let msg = string_until_zero_expected(&mut cursor, "k");
+
+        assert!(!msg.is_none());
+        assert_eq!(msg.unwrap(), "v");
+    }
+
+    #[test]
+    fn test_parse_plugins() {
+        let plugins = parse_plugins(None);
+        assert_eq!(plugins.0, "");
+        assert_eq!(plugins.1.len(), 0);
+
+        let plugins = parse_plugins(Some("CraftBukkit on Bukkit 1.2.5-R4.0".to_string()));
+        assert_eq!(plugins.0, "CraftBukkit on Bukkit 1.2.5-R4.0");
+        assert_eq!(plugins.1.len(), 0);
+
+        let plugins = parse_plugins(Some(
+            "CraftBukkit on Bukkit 1.2.5-R4.0: WorldEdit 5.3; CommandBook 2.1".to_string(),
+        ));
+        assert_eq!(plugins.0, "CraftBukkit on Bukkit 1.2.5-R4.0");
+        assert_eq!(plugins.1, vec!["WorldEdit 5.3", "CommandBook 2.1"]);
+    }
+
+    #[test]
+    fn test_parse_players() {
+        let mut cursor = std::io::Cursor::new(vec![97, 0, 98, 0, 99, 0, 0]);
+        let players = parse_players(&mut cursor);
+
+        assert_eq!(players, vec!["a", "b", "c"]);
+    }
+
+    #[test]
     fn test_send_ping() {
         match send_ping("ping.minecraft.syfaro.net", 25565) {
             Ok(ping) => println!("{:?}", ping),
             Err(_) => assert!(false, "should not error"),
         }
 
-        match send_ping("Play.AspiriaMc.Com", 25565) {
+        match send_ping("s.nerd.nu", 25565) {
             Ok(ping) => println!("{:?}", ping),
             Err(_) => assert!(false, "should not error"),
+        }
+    }
+
+    #[test]
+    fn test_send_query() {
+        match send_query("127.0.0.1", 25565) {
+            Ok(query) => println!("{:?}", query),
+            Err(_e) => assert!(false, "should not error"),
         }
     }
 }
